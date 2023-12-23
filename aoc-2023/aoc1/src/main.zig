24d6497dcdcfc6cb4850c93490dc5d129be14521
unknown <claponsie@gmail.com>
2023-12-23 11:56:04 -0500

    Two stars baby

--- aoc-2023/aoc1/src/main.zig
+++ aoc-2023/aoc1/src/main.zig
@@ -15,7 +15,10 @@ pub fn load_input() !void {
 
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
+        try parse_numeric_words(line);
         var digits = try extract_digits(line);
+
+        std.debug.print("{d} = {s}\n", .{ digits, line });
         sum = sum + digits;
     }
     std.debug.print("{d}\n", .{sum});
@@ -45,4 +48,27 @@ pub fn extract_digits(line: []u8) !u8 {
     // must be a 2 digit number line
     var two_digit = start * 10 + end;
     return two_digit;
+}
+
+pub fn parse_numeric_words(line: []u8) !void {
+    var words = [_][]const u8{ "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+    for (line, 0..) |char, j| {
+        for (words, 0..) |word, i| {
+            var seek_char = word[0];
+            var word_length = word.len;
+
+            if (char != seek_char) continue;
+            if (j + word_length > line.len) break;
+
+            var slice0 = line[j .. j + word_length];
+            var slice1 = word[0..word.len];
+            if (std.mem.eql(u8, slice0, slice1)) {
+                //std.debug.print("Matched {s} = {d}: {s}\n", .{ word, i, line });
+                // replace first letter of word with number... slight hack to avoid reallocating memory for now.
+                var offset: u8 = @truncate(i);
+                line[j] = 48 + offset;
+            }
+        }
+    }
+    return;
 }
